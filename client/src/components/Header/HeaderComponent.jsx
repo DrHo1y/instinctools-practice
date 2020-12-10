1d6625be0dfeb1588083697a3eee5dfe77170f26
Владимир Свербинский <vladimirsverbinskiy@MacBook-Pro-Vladimir.local>
2020-12-10 21:05:41 +0300

    style bottom panel on the header

--- client/src/components/Header/HeaderComponent.jsx
+++ client/src/components/Header/HeaderComponent.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import { theme, Header, HeaderWrapperSecond } from '../../styles/styles'
+import {
+  theme,
+  Header,
+  HeaderWrapperSecond,
+  Img,
+  HeaderWrapperElement,
+  HeaderWrapperElementTitle,
+} from '../../styles/styles'
 import HeaderFirst from './HeaderFirst/HeaderFirst'
+import HeaderSecond from './HeaderSecond/HeaderSecond'
 
 const HeaderComponent = (props) => {
   return (
     <Header color={theme.primaryColor}>
       <HeaderFirst />
-      <HeaderWrapperSecond>2</HeaderWrapperSecond>
+      <HeaderSecond />
     </Header>
   )
 }
--- client/src/styles/styles.js
+++ client/src/styles/styles.js
@@ -53,7 +53,52 @@ export const HeaderWrapperFirstControlWrapper = styled.div`
 `
 
 export const HeaderWrapperSecond = styled.div`
-  padding: 22px 0px 2px 5px;
+  padding: 22px 0 2px 0;
+`
+
+export const HeaderWrapperElement = styled.div`
+  opacity: ${(props) => (props.active ? '1' : '0.2')};
+  margin-right: 40px;
+  height: 36px;
+  display: inline-flex;
+  cursor: pointer;
+  position: relative;
+  transition: all 0.3s ease;
+  &::before {
+    content: '';
+    display: ${(props) => (props.active ? 'block' : 'none')};
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    background-color: white;
+    bottom: 0;
+  }
+  &:hover {
+    opacity: 1;
+    &::before {
+      content: '';
+      display: block;
+      position: absolute;
+      width: 100%;
+      height: 2px;
+      background-color: white;
+      bottom: 0;
+    }
+  }
+`
+const img = ({ src, alt }) => (
+  <img src={src} alt={alt} style={{ width: 24, height: 24 }} />
+)
+export const Img = styled(img)`
+  width: 24px;
+  height: 24px;
+  color: ${(props) => props.color || 'white'};
+`
+export const HeaderWrapperElementTitle = styled.h2`
+  padding: 4px 5px;
+  font-weight: 600;
+  font-size: 16px;
+  color: white;
 `
 
 export const Button = styled.div`
@@ -63,10 +108,10 @@ export const Button = styled.div`
   border: ${(props) => props.border || 'none'};
   background: ${(props) => props.color};
   color: ${(props) => props.textColor};
-  font-family: 'Montserrat', sans-serif;
   font-size: ${(props) => props.fontSize || '16px'};
   font-weight: ${(props) => props.fontWeight || '600'};
   cursor: pointer;
+  transition: all 0.3s ease;
   &:hover {
     color: ${theme.secondaryColor};
   }
