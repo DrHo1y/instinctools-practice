edaf4f5a3aa445e38021051f132f97de2453711d
Владимир Свербинский <vladimirsverbinskiy@MacBook-Pro-Vladimir.local>
2020-12-11 15:22:58 +0300

    qwe

--- client/src/components/Header/HeaderComponent.jsx
+++ client/src/components/Header/HeaderComponent.jsx
@@ -1,12 +1,5 @@
 import React from 'react'
-import {
-  theme,
-  Header,
-  HeaderWrapperSecond,
-  Img,
-  HeaderWrapperElement,
-  HeaderWrapperElementTitle,
-} from '../../styles/styles'
+import { theme, Header } from '../../styles/styles'
 import HeaderFirst from './HeaderFirst/HeaderFirst'
 import HeaderSecond from './HeaderSecond/HeaderSecond'
 
